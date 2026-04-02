060b4e78a744360b6ff42620d3bd113e5cc68e7c
Rumana-kaladgi <rumanakaladgi@gmail.com>
2026-04-02 15:24:23 +0530

    Update JavaFile.java

--- JavaFile.java
+++ JavaFile.java
@@ -1,5 +1,6 @@
 public class JavaFile{
     public static void main(String[] args){
         System.out.println("Welcome to Java");
+        System.out.println("Java is a Programming Language");
     }
 }
